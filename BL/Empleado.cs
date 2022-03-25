51519abaef0fedf0727c892af414d7096bd94d9c
Fer <fercholuna14@outlook.es>
2022-03-25 17:09:18 -0600

    Es el primer commit

---
+++ BL/Empleado.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace BL
+{
+    public class Empleado
+    {
+        public static ML.Result GetAll()
+        {
+            
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(DL.Conexion.Connection()))
+                {
+                    con.Open();
+                    string sql = "SELECT * FROM entrada";
+                    MySqlCommand cmd = new MySqlCommand(sql, con);
+                    MySqlDataReader rdr = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(rdr);
+                    //int numberOfResults = dt.Rows.Count;
+                    //while (rdr.Read())
+                    //{
+                    if (dt.Rows.Count > 0)
+                    {
+                        result.Objects = new List<object>();
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            ML.Entrada entrada = new ML.Entrada();
+                            entrada.IdEntrada = Convert.ToInt16(row["IdEntrada"]);
+                            entrada.Descripcion = row["Descripcion"].ToString();
+                            result.Objects.Add(entrada);
+                        }
+                        result.validacion = true;
+                    }else
+                    {
+                        result.validacion = false;
+                        result.Message = "No hay registro alguno";
+                    }
+                    
+                      
+                    //}
+                    rdr.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                result.validacion = false;
+                result.Message = ex.Message;
+            }
+            return result;
+           
+        }
+
+    }
+}
---
+++ DL/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using System.Configuration;
+
+namespace DL
+{
+    public class Conexion
+    {
+        public static string Connection()
+        {
+            string conexion = System.Configuration.ConfigurationManager.ConnectionStrings["FLunaConnection"].ConnectionString;
+            return conexion;
+        }
+    }
+}
